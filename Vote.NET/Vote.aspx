a105b1bfe236b23f6f7413a25dc2904837f9db15
1938917626@qq.com <1938917626@qq.com>
2019-10-18 20:46:50 +0800

    小bug未更改

---
+++ Vote.NET/Vote.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Vote.aspx.cs" Inherits="Vote.NET.Vote" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="新闻人物投票系统"></asp:Label>
+            
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td>
+            <asp:Label ID="Label2" runat="server" Text="注意："></asp:Label>
+            <asp:Label ID="lblState" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:RadioButtonList ID="rbtlVote" runat="server" AutoPostBack="True" CausesValidation="True" RepeatColumns="2">
+                        <asp:ListItem>张玉杰</asp:ListItem>
+                        <asp:ListItem>李明达</asp:ListItem>
+                        <asp:ListItem>王瑜兰</asp:ListItem>
+                        <asp:ListItem>赵志奇</asp:ListItem>
+                        <asp:ListItem>马伟明</asp:ListItem>
+                        <asp:ListItem>程超</asp:ListItem>
+                        <asp:ListItem>刘平真</asp:ListItem>
+                        <asp:ListItem>张群英</asp:ListItem>
+                        <asp:ListItem>王子文</asp:ListItem>
+                        <asp:ListItem>杨波</asp:ListItem>
+                    </asp:RadioButtonList>
+                    
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnVote" runat="server" OnClick="btnVote_Click" Text="投票" />
+                    <asp:Button ID="btnView" runat="server" Text="查看" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblView" runat="server"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ Vote.NET/Vote.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+
+namespace Vote.NET
+{
+    public partial class Vote : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblView.Text = "";
+            HttpCookie getCookie = Request.Cookies["Vote"];//读cookie
+            if(getCookie == null)
+            {
+                lblState.Text = "您还未投票";
+            }
+            else
+            {
+                lblState.Text = "您已经投过票了";
+            }
+            GetVote();//读取vote.txt文件
+        }
+        ArrayList count = new ArrayList();
+        //定义读取文件的方法
+        protected void GetVote()
+        {
+            string filepath = Server.MapPath("vote.txt");
+            //if (!File.Exists(Server.MapPath("vote.txt")))
+            //{
+            //    File.CreateText(Server.MapPath("vote.txt"));
+            //}
+            StreamReader sr = File.OpenText(filepath);
+            while (sr.Peek() != -1)
+            {
+                string str = sr.ReadLine();
+                string[] strVote = str.Split('l');
+                foreach (string ss in strVote)
+                    count.Add(int.Parse(ss));
+            }
+            sr.Close();
+        }
+        //定义写文件的方法
+        protected void PutVote()
+        {
+            string filepath = Server.MapPath("vote.txt");
+            //if (!File.Exists(Server.MapPath("vote.txt")))
+            //{
+            //    File.CreateText(Server.MapPath("vote.txt"));
+            //}
+            StreamWriter sw = new StreamWriter(filepath);
+            string str = count[0].ToString();
+            for(int i = 1; i < count.Count; i++)
+            {
+                str += "l" + count[i].ToString();
+            }
+            sw.WriteLine(str);
+            sw.Close();
+        }
+
+        protected void btnVote_Click(object sender, EventArgs e)
+        {
+            if (rbtlVote.SelectedIndex != -1)
+            {
+                //防止重复投票
+                HttpCookie getCookie = Request.Cookies["Vote"];
+                if (getCookie != null)
+                {
+                    Response.Write("<script>alert('你已经投过票了，不能重复投票');" +
+                        "location='javascript:history.go(-1)'</script>");
+                }
+                else
+                {
+                    int k = rbtlVote.SelectedIndex;
+                    //报错
+                    count[k] = int.Parse(count[k].ToString())+1;
+                    PutVote();
+                    HttpCookie vCookie = new HttpCookie("vote");
+                    vCookie.Value = "vote";
+                    vCookie.Expires = DateTime.Now.AddDays(1);
+                    Response.Write("<script>alert('投票成功');</script>");
+                }
+            }
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            lblView.Text = "各候选人票数：<br/>";
+            for(int i = 0; i < rbtlVote.Items.Count; i++)
+            {
+                lblView.Text += rbtlVote.Items[i].Value + ":" + "票" + "<br/>";
+            }
+        }
+    }
+}
